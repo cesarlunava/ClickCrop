a24abb6716443affba56e7839746c26fa6a6300e
cesarlunava <a01254603@tec.mx>
2024-12-08 21:11:37 -0700

    archivos .js

--- app.js
+++ app.js
@@ -5,4 +5,28 @@ const port = 3000
 const mysql = require('mysql2/promise');
 const cors = require('cors')
 const session = require('express-session')
-const md5 = require('md5')
+const md5 = require('md5');
+const bcrypt = require('bycrypt');
+const login = require('./log-in');
+const saltRounds = 10;
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
+app.use(session({
+    secret: 'ashuby23tv4h32v4uwd78vus87sd8dfhjkig98'
+}))
+
+const connection = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'login',
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.get('/log-in', login)
+    
---
+++ log-in.js
@@ -0,0 +1,25 @@
+const login = async (req, res) => {
+    const datos = req.query
+
+    try {
+        const [results, fields] = await connection.query(
+            "SELECT * FROM `usuarios` WHERE `usuario` = ?",
+            [datos.usuario]
+        );
+        console.log(bcrypt.hashSync(datos.clave, saltRounds));
+        if (results.length > 0 && bcrypt.compareSync(datos.clave, results[0].clave)) {
+            req.session.usuario = datos.usuario;
+            res.status(200).send('Inicio de sesión correcto')
+        } else {
+            res.status(401).send('Datos incorrectos')
+        }
+
+        console.log(results);
+        console.log(fields);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Error en el servidor')
+    }
+}
+
+module.exports = login;
